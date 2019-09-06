68223cfed2844d577bc75f0bcbb3bb56faf21625
Daniel Hoo <danielhoo.chn@gmail.com>
2019-09-06 11:04:24 +0800

    first commit

---
+++ docs/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "description": "lecu server api doc",
+  "title": "Lecu Server API",
+  "name": "lecu-server",
+  "version": "0.1.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-06T02:33:34.358Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
